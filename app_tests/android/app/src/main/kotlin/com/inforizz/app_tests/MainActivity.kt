b62a7aeb7e6019c74b287b0148e2b2632f00b190
agipensador <agipensador@gmail.com>
2024-05-14 11:01:36 -0300

    iniciando o projeto

---
+++ app_tests/android/app/src/main/kotlin/com/inforizz/app_tests/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.inforizz.app_tests
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ app_tests/lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:app_tests/app/screens/register_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp( ));
+}
+
