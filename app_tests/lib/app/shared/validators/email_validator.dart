ab037bfa0b19bb29073b1193a212555ed83749d6
agipensador <agipensador@gmail.com>
2024-05-14 13:10:36 -0300

    testes EMAIL funcionando, classe de EMAIL VALIDATOR movida para PRODUCAO.

---
+++ app_tests/lib/app/shared/validators/email_validator.dart
@@ -0,0 +1,21 @@
+//CÓDIGO DE PRODUÇÃO
+class EmailValidator {
+  validate({String? email}) {
+    // teste 1       || teste 2
+    if (email == null || email.isEmpty) {
+      return 'Email obrigatório';
+    }
+
+    //teste 3 e 4
+    //emailRegex É UM CÓDIGO REGEX PADRÃO QUE VERIFICA OS CARACTERES QUE SÃO VÁLIDOS PARA EMAIL
+    final emailRegex = RegExp(r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+");
+    //VERIFICA SE O EMAIL QUE PASSAMOS ESTÁ EM >DESACORDO< COM OS CARACTERES DE EMAIL VÁLIDO
+    if(emailRegex.hasMatch(email)){
+      //DEVE DAR ERRO PQ A VERIFICAÇÃO É SE FOR INVÁLIDO
+      return 'Email válido';
+    }else {
+      //O TESTE É APROVADO POIS É ISTO QUE ELE ESPERA
+      return 'Email inválido';
+    }
+  }
+}
