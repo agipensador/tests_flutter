07f320a7fe752233cf22cd3a022f1664e64c4f1e
agipensador <agipensador@gmail.com>
2024-05-14 11:30:55 -0300

    Primeiro RUN nos testes, retornando o erro de que o retorno foi nulo e precisava de uma string com frase de Email obrigatório

---
+++ app_tests/test/app/shared/validators/email_validator_test.dart
@@ -0,0 +1,26 @@
+import 'package:flutter_test/flutter_test.dart';
+
+//CLASSE CRIADA PARA REPRESENTAR O CÓDIGO DE PRODUÇÃO
+class EmailValidator{
+  validate(){}
+}
+
+//INICIALIZADOR DE TESTE
+void main(){
+  late EmailValidator emailValidator;
+
+  //FUNÇÃO SETUP PARA REUTILIZAR O QUE SE REPETE EM DIVERSAS >FUNÇÔES DE TEST<
+  setUp(() => emailValidator = EmailValidator());
+
+  //GRUPO DE TESTES
+  group(
+      //O QUE ESSE GRUPO FARÁ?
+      'Validar Email', () {
+      //O QUE ESSA FUNÇÃO TEST FARÁ?
+      test('Retornar erro caso email igual nulo', () {
+        final result = emailValidator.validate();
+        //VERIFICA SE O RESULTADO ESPERRADO É IGUAL A: Email obrigatório
+        expect(result, equals('Email obrigatório'));
+      });
+  });
+}
