a258692c4ad74daa02b0a728e947d8225aee25f0
agipensador <agipensador@gmail.com>
2024-05-14 15:18:11 -0300

    classe AUTH SERVICE adicionada ao package LIB

--- app_tests/test/app/shared/auth/auth_service_test.dart
+++ app_tests/test/app/shared/auth/auth_service_test.dart
@@ -1,20 +1,5 @@
-import 'package:app_tests/app/shared/validators/email_validator.dart';
-import 'package:app_tests/app/shared/validators/pass_validator.dart';
+import 'package:app_tests/app/shared/auth/auth_service.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-class Auth {
-  register(String? emailAuth, String? passAuth) {
-    //BUSCA NA CLASSE EmailValidator O RESULTADO DA VERIFICAÇÃO
-    final emailError = EmailValidator().validate(email: emailAuth);
-
-    //BUSCA NA CLASSE PassValidator O RESULTADO DA VERIFICAÇÃO
-    final passError = PassValidator().validate(pass: passAuth);
-
-    //VERIFICA SE O EMAILERROR É NULO, SE NÃO FOR ENTÃO VERIFICA SE PASSERROR É NULO.
-    //CASO OS 2 ESTEJAM OK, RETORNA NULO
-    return emailError ?? passError;
-  }
-}
 
 main() {
   late Auth auth;
