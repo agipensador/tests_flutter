0c57f6d1f3ba36282289664f8830908e79dc0c50
agipensador <agipensador@gmail.com>
2024-05-18 03:44:56 -0300

    Testes 2 e 3 sobre validacao de EMAIL estao prontos

--- app_tests/test/app/screens/register_screen_test.dart
+++ app_tests/test/app/screens/register_screen_test.dart
@@ -10,22 +10,72 @@ void main() {
 
      //TESTE 1
     //O QUE ESSA FUNÇÃO TESTWIDGET FARÁ?
-    testWidgets("Confirmar que o campo Email aparece corretamente na tela",
-        //PASSAMOS UM PARÂMETRO/VARIÁVEL PARA UTILIZAR NA FUNÇÃO(widgetTester)
+    testWidgets("Confirmar que o campo TextForm de Email aparece corretamente na tela",
+
+        //PARÂMETRO/VARIÁVEL PARA UTILIZAR NA FUNÇÃO(widgetTester)
         (widgetTester) async {
 
-      //pumpWidget RENDERIZA TODA A TELA, INICIALIZANDO O MATERIAL E O REGISTER
-      await widgetTester.pumpWidget(
+          //pumpWidget RENDERIZA TODA A TELA, INICIALIZANDO O MATERIAL E O REGISTER
+          await widgetTester.pumpWidget(
 
           //IMPORTANTE O MATERIA APP PQ A TELA REGISTERSCREEN NÃO POSSUI O MATERIALAPP
           //E PARA TELA FLUTTER BUILDAR É NECESSÁRIO O MATERIAL
           const MaterialApp(home: RegisterScreen()));
 
-      //PARA RECUPERAR A CHAVE DE EMAIL PRECISA SER EXATAMENTE O MESMO TEXTO DA KEY DECLARADA "email"
+          //PARA RECUPERAR A CHAVE DE EMAIL PRECISA SER EXATAMENTE O MESMO TEXTO DA KEY DECLARADA "email"
           final emailKey = find.byKey(const Key("email"));
 
           //VERIFICA emailKey SE ESTÁ REALMENTE SENDO EXIBIDO NA TELA
           expect(emailKey, findsOneWidget);
     });
+
+    //TESTE 1
+    //O QUE ESSA FUNÇÃO TESTWIDGET FARÁ?
+    testWidgets("Confirmar que o campo TextForm de Pass aparece corretamente na tela",
+
+        //PARÂMETRO/VARIÁVEL PARA UTILIZAR NA FUNÇÃO(widgetTester)
+            (widgetTester) async {
+
+          //pumpWidget RENDERIZA TODA A TELA, INICIALIZANDO O MATERIAL E O REGISTER
+          await widgetTester.pumpWidget(
+
+            //IMPORTANTE O MATERIA APP PQ A TELA REGISTERSCREEN NÃO POSSUI O MATERIALAPP
+            //E PARA TELA FLUTTER BUILDAR É NECESSÁRIO O MATERIAL
+              const MaterialApp(home: RegisterScreen()));
+
+          //PARA RECUPERAR A CHAVE DE PASS PRECISA SER EXATAMENTE O MESMO TEXTO DA KEY DECLARADA "pass"
+          final passKey = find.byKey(const Key("pass"));
+
+          //VERIFICA passKey SE ESTÁ REALMENTE SENDO EXIBIDO NA TELA
+          expect(passKey, findsOneWidget);
+        });
+
+
+    //TESTE 3
+    //O QUE ESSA FUNÇÃO TESTWIDGET FARÁ?
+    testWidgets("Exibir SnackBar caso o EMAIL seja nulo",
+
+            //PARÂMETRO/VARIÁVEL PARA UTILIZAR NA FUNÇÃO(widgetTester)
+            (widgetTester) async {
+
+            //pumpWidget RENDERIZA TODA A TELA, INICIALIZANDO O MATERIAL E O REGISTER
+            await widgetTester.pumpWidget(
+
+              //IMPORTANTE O MATERIA APP PQ A TELA REGISTERSCREEN NÃO POSSUI O MATERIALAPP
+              //E PARA TELA FLUTTER BUILDAR É NECESSÁRIO O MATERIAL
+              const MaterialApp(home: RegisterScreen()));
+
+            //PARA RECUPERAR A CHAVE DE REGISTER PRECISA SER EXATAMENTE O MESMO TEXTO DA KEY DECLARADA "register"
+            final button = find.byKey(const Key("register"));
+
+            //BUSCA E CLICA NO BOTÃO COM KEY register
+            await widgetTester.tap(button);
+            //pumpAndSettle RENDERIZA A TELA APÓS O CLIQUE E DEIXA PRONTA PRO PRÓXIMO COMANDO/TESTE
+            await widgetTester.pumpAndSettle();
+
+            //VERIFICA SE EXISTE O CASO DE ERRO Email obrigatório (TO UPPERCASE(MAIÚSCULO))
+            //findsOneWidget SIGNIFICA 1 WIDGET QUE CONTENHA ISSO
+            expect(find.text("Email obrigatório".toUpperCase()), findsOneWidget);
+        });
   });
 }
