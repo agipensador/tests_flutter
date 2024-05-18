741a1ea9c7ffb91923f00ddb0081b3b15394ce5d
agipensador <agipensador@gmail.com>
2024-05-18 01:36:03 -0300

    PERCEBA: O Teste unitário pode ser feito diretamente através da linguagem DART, por isto a tela vermelha!! Precisamos criar nosso MaterialApp ;P kk

--- app_tests/lib/app/screens/register_screen.dart
+++ app_tests/lib/app/screens/register_screen.dart
@@ -9,6 +9,7 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   final TextEditingController emailTextEditingController = TextEditingController();
+  final TextEditingController passTextEditingController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -17,20 +18,34 @@ class _MyAppState extends State<MyApp> {
         title: const Text('Flutter Testes'),
       ),
       body: SingleChildScrollView(
+        padding: const EdgeInsets.all(12),
         child: Column(
           children: [
             TextFormField(
-              //O TESTE LOCALIZA ESTE WIDGET POR CONTA DESTA KEY "email"
+              //O TESTE LOCALIZA ESTE WIDGET POR CONTA DA KEY "email"
               key: const Key('email'),
-              controller: emailTextEditingController ,
+              controller: emailTextEditingController,
               keyboardType: TextInputType.emailAddress,
               decoration: const InputDecoration(
-                  hintText: 'Email',
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(12)),
-                  ),
+                hintText: 'Email',
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(12)),
+                ),
               ),
-            )
+            ),
+            const SizedBox(height: 8),
+            TextFormField(
+              //O TESTE LOCALIZA ESTE WIDGET POR CONTA DA KEY "pass"
+              key: const Key('pass'),
+              controller: passTextEditingController,
+              keyboardType: TextInputType. visiblePassword,
+              decoration: const InputDecoration(
+                hintText: 'Senha',
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(12)),
+                ),
+              ),
+            ),
           ],
         ),
       ),
--- app_tests/lib/main.dart
+++ app_tests/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:app_tests/app/screens/register_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp( ));
+  runApp(const MaterialApp(
+    home: MyApp(),
+  ));
 }
-
